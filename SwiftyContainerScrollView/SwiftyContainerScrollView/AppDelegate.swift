626d28b4b9f8ca52120237f50480693104755903
murphy <lj472840101@163.com>
2020-06-29 22:34:05 +0800

    initial commit

---
+++ SwiftyContainerScrollView/SwiftyContainerScrollView/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SwiftyContainerScrollView
+//
+//  Created by Murphy_Zeng on 2020/6/29.
+//  Copyright © 2020 murphy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
